2e27e62fe5c98e0450b47615668cde5a4f2cd8d7
AlexBrown4020 <balex3497@gmail.com>
2022-09-12 20:37:00 +0900

    Feat: Implement basic CRUD functionality for lessons

--- api/routes/lessons.js
+++ api/routes/lessons.js
@@ -14,4 +14,40 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedLesson = await Lesson.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+        res.status(200).json(updatedLesson);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await Lesson.findByIdAndDelete(req.params.id);
+        res.status(200).json('Lesson deleted.');
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const lesson = await Lesson.findById(req.params.id);
+        res.status(200).json(lesson);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const lessons = await Lesson.find();
+        res.status(200).json(lessons);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 export default router;

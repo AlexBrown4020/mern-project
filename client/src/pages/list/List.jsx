3be8b1484477399cdacfaf0707767eaa265fa4ce
AlexBrown4020 <balex3497@gmail.com>
2022-09-13 20:09:30 +0900

    Feat: Add proxy for API call for lessons, Fix naming of Home file

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch';
 
 export const List = () => {
+
+    const {data, loading, error} = useFetch('/lessons');
+    console.log(data)
     return (
         <div>
             List

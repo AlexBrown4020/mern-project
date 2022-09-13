b8853ba1862fbdbd8e8cdca6c496abb7341d4059
AlexBrown4020 <balex3497@gmail.com>
2022-09-13 20:04:29 +0900

    Feat: Add fetch requests for api data

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,9 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const useFetch = (url) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
-    
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try{
+                const response = await axios.get(url);
+                setData(response.data)
+            } catch(err) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+    }, [url]);
+
+    const reFetch = async () => {
+        setLoading(true);
+        try{
+            const response = await axios.get(url);
+            setData(response.data)
+        } catch(err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+    return {data, loading, error, reFetch};
 };
+
+export default useFetch

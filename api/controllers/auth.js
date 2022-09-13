56959341245f0a1cd91535a95766d086fbc15839
AlexBrown4020 <balex3497@gmail.com>
2022-09-13 13:15:09 +0900

    Feat: Implement bcrypt for password hashing on user authentication

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,10 +1,14 @@
-import User from '../models/User.js'
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
-export const register  = async (req, res, next) => {
+const register  = async (req, res, next) => {
     try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
-            password: req.body.password,
+            password: hash,
             email: req.body.email,
         });
     await newUser.save();
@@ -14,3 +18,5 @@ export const register  = async (req, res, next) => {
         next(err);
     }
 };
+
+export default register;

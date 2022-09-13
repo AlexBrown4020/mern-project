37ce352a03493777c4ebe810dad63f775c326e16
AlexBrown4020 <balex3497@gmail.com>
2022-09-13 17:37:05 +0900

    Feat: Implement home routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/home/home';
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

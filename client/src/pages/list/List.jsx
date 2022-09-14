c0bb53e60b93b3933ec66ff4a16a4129909fce83
AlexBrown4020 <balex3497@gmail.com>
2022-09-14 10:15:31 +0900

    Feat: Implement List of all lesson data

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import useFetch from '../../hooks/useFetch';
+import './list.css';
 
 export const List = () => {
     const {data, loading, error} = useFetch('/lessons');
@@ -7,7 +8,24 @@ export const List = () => {
     
     return (
         <div>
-            List
+            {loading ? (
+                'Loading, please wait.'
+                ) : (
+                <div>
+                    {data.map(lesson => {
+                        return <div className='lessonContainer'>
+                                <div className='lessonTitle'>
+                                    <h2>{lesson.title}</h2>
+                                </div>
+                                <div className='lessonContent'>
+                                    <p>Date: {lesson.date.slice(0,10)}</p>
+                                    <p>Time: {lesson.date.slice(11,19)}</p>
+                                    <p>{lesson.description}</p>
+                                </div>
+                            </div>
+                    })}
+                </div>
+                )}
         </div>
     )
 };

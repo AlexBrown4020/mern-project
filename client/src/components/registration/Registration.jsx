d74a8be5fdb8d4f288acd32f1c080e5937c42e49
AlexBrown4020 <balex3497@gmail.com>
2022-09-14 13:11:39 +0900

    Feat: Implement low fidelity sign up, check it returns expected data

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../navbar/Navbar';
 import './registration.css';
 
 export const Registration = () => {
+    const [name, setName]=useState('');
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=('');
+
+    const submitData = () => {
+        console.log(name, email, password)
+    }
+
     return (
         <div>
             <Navbar/>
             <div className='registrationPage'>
                 <h2>Register</h2>
                 <div className='formContainer'>
-                    <input className='submissionInput' type='text' placeholder='Enter username' />
-                    <input className='submissionInput' type='password' placeholder='Enter password' />
-                    <input className='submissionInput' type='email' placeholder='Enter email' />
-                    <button className='submissionButton' type='button'>Submit</button>
+                    <input className='submissionInput' type='text' placeholder='Enter username' 
+                        value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input className='submissionInput' type='password' placeholder='Enter password' 
+                        value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input className='submissionInput' type='email' placeholder='Enter email' 
+                        value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <button onClick={submitData} className='submissionButton' type='button'>Submit</button>
                 </div>
             </div>
         </div>

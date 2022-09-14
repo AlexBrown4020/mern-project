700cef0099e048e101a744e4dd28261c06fa8bb3
AlexBrown4020 <balex3497@gmail.com>
2022-09-14 13:05:01 +0900

    Feat: Add styling to registration form

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Navbar } from '../navbar/Navbar';
+import './registration.css';
 
 export const Registration = () => {
     return (
         <div>
             <Navbar/>
-            <h2>Register</h2>
-            <div className='formContainer'>
-                <input className='submissionInput' type='text' placeholder='Enter username' />
-                <input className='submissionInput' type='password' placeholder='Enter password' />
-                <input className='submissionInput' type='email' placeholder='Enter email' />
-                <button className='submissionButton' type='button'>Submit</button>
+            <div className='registrationPage'>
+                <h2>Register</h2>
+                <div className='formContainer'>
+                    <input className='submissionInput' type='text' placeholder='Enter username' />
+                    <input className='submissionInput' type='password' placeholder='Enter password' />
+                    <input className='submissionInput' type='email' placeholder='Enter email' />
+                    <button className='submissionButton' type='button'>Submit</button>
+                </div>
             </div>
         </div>
     )

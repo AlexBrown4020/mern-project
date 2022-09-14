28dace12f34b74523c57aa3a112c3f658d7e0d8a
AlexBrown4020 <balex3497@gmail.com>
2022-09-14 12:56:31 +0900

    Feat: add basic form submission parameters

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -7,7 +7,10 @@ export const Registration = () => {
             <Navbar/>
             <h2>Register</h2>
             <div className='formContainer'>
-
+                <input className='submissionInput' type='text' placeholder='Enter username' />
+                <input className='submissionInput' type='password' placeholder='Enter password' />
+                <input className='submissionInput' type='email' placeholder='Enter email' />
+                <button className='submissionButton' type='button'>Submit</button>
             </div>
         </div>
     )

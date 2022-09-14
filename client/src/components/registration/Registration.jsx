96b19ac3e87347566e7ca79ad35b51c5c4571dde
AlexBrown4020 <balex3497@gmail.com>
2022-09-14 14:32:22 +0900

    fix: Attempt to solve failure to fetch at submitData

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import { Navbar } from '../navbar/Navbar';
 import './registration.css';
-import axios from 'axios';
 
 export const Registration = () => {
-    const [name, setName]=useState('');
+    const [username, setName]=useState('');
     const [email, setEmail]=useState('');
     const [password, setPassword]=useState('');
 
     const submitData = async () => {
-        let result = await axios.fetch('http://localhost:8000/users/', {
+        let result = await fetch('http://localhost:8000/users', {
             method:'post',
             body: JSON.stringify({
-                name, email, password
+                username, email, password
             }),
             headers: {
                 'Content-Type':'application/json'
@@ -29,7 +28,7 @@ export const Registration = () => {
                 <h2>Register</h2>
                 <div className='formContainer'>
                     <input className='submissionInput' type='text' placeholder='Enter username' 
-                        value={name} onChange={(e) => setName(e.target.value)}/>
+                        value={username} onChange={(e) => setName(e.target.value)}/>
                     <input className='submissionInput' type='password' placeholder='Enter password' 
                         value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <input className='submissionInput' type='email' placeholder='Enter email' 

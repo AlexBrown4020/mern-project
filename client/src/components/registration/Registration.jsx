ecf92d87186aa2120beb4ca68ca32f6020183508
AlexBrown4020 <balex3497@gmail.com>
2022-09-14 12:53:57 +0900

    Feat: Implement Navbar for registration page

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Navbar } from '../navbar/Navbar';
 
 export const Registration = () => {
     return (
         <div>
+            <Navbar/>
             <h2>Register</h2>
             <div className='formContainer'>
 

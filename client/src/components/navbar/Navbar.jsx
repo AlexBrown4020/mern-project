e99c1265ef9953b0727edcdb97de1748d6e7f9bc
AlexBrown4020 <balex3497@gmail.com>
2022-09-15 14:16:03 +0900

    Feat: logout and register conditional rendering

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Registration } from '../registration/Registration';
 import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
-
+    const auth = localStorage.getItem('user')
     return (
         <div className='navbar'>
             <div className='navContainer'>
@@ -12,7 +12,7 @@ export const Navbar = () => {
                 <ul className='navItems'>
                     <Link className='navLink' to={'/create_lesson'}>Create Lesson</Link>
                     <Link className='navLink' to={'/login'}>Login</Link>
-                    <Link className='navLink'to={'/register'}>Register</Link>
+                    {auth ? <Link className='navLink' to={'/logout'}>Logout</Link> : <Link className='navLink'to={'/register'}>Register</Link>}
                 </ul>
             </div>
         </div>

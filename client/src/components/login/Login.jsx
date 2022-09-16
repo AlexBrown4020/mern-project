897ea9a72c6beb1601ace4211c1d0b8e45f4b417
AlexBrown4020 <balex3497@gmail.com>
2022-09-16 11:42:58 +0900

    Feat: Include email criteria, prevent 500 error

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -5,9 +5,22 @@ import { Navbar } from '../navbar/Navbar';
 export const Login = () => {
 const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
-const handleLogin = () => {
-    console.warn(username, password)
-}
+const [email, setEmail] = useState('');
+
+const submitData = async () => {
+    let result = await fetch('http://localhost:8000/users', {
+        method:'post',
+        body: JSON.stringify({
+            username, email, password
+        }),
+        headers: {
+            'Content-Type':'application/json'
+        }
+    });
+    result = await result.json();
+    console.log(result)
+    localStorage.setItem('user', JSON.stringify(result))
+};
 
     return (
         <div>
@@ -17,9 +30,11 @@ const handleLogin = () => {
                 <div className='formContainer'>
                     <input className='submissionInput' type='text' placeholder='Enter Username' onChange={(e) => {
                         setUsername(e.target.value)}} value={username}></input>
+                    <input className='submissionInput' type='email' placeholder='Enter Email' onChange={(e) => {
+                        setEmail(e.target.value)}} value={email}></input>
                     <input className='submissionInput' type='password' placeholder='Enter Password' onChange={(e) => {
                         setPassword(e.target.value)}} value={password}></input>
-                    <button onClick={handleLogin} className='submissionButton' type='button'>Login</button>
+                    <button onClick={submitData} className='submissionButton' type='button'>Login</button>
                 </div>
             </div>
         </div>

b062770f761b1ab4c1f71a9765613eb62de4d95a
AlexBrown4020 <balex3497@gmail.com>
2022-09-16 11:32:09 +0900

    Feat: Implement state changes for login credentials

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../navbar/Navbar';
 
 
 export const Login = () => {
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const handleLogin = () => {
+    console.warn(email, password)
+}
+
     return (
         <div>
             <Navbar/>
             <div className='registrationPage'>
                 <h2>Login</h2>
                 <div className='formContainer'>
-                    <input className='submissionInput' type='test' placeholder='Enter Email'></input>
-                    <input className='submissionInput' type='test' placeholder='Enter Password'></input>
-                    <button className='submissionButton' type='button'>Login</button>
+                    <input className='submissionInput' type='test' placeholder='Enter Email' onChange={(e) => {
+                        setEmail(e.target.value)}} value={email}></input>
+                    <input className='submissionInput' type='test' placeholder='Enter Password' onChange={(e) => {
+                        setPassword(e.target.value)}} value={password}></input>
+                    <button onClick={handleLogin} className='submissionButton' type='button'>Login</button>
                 </div>
             </div>
         </div>

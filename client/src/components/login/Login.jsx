31014ec4553c7288aee0bfb35c66439411d72a7e
AlexBrown4020 <balex3497@gmail.com>
2022-09-16 11:36:14 +0900

    Feat: Change lookup criteria to unique key: username

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -3,10 +3,10 @@ import { Navbar } from '../navbar/Navbar';
 
 
 export const Login = () => {
-const [email, setEmail] = useState('');
+const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
 const handleLogin = () => {
-    console.warn(email, password)
+    console.warn(username, password)
 }
 
     return (
@@ -15,9 +15,9 @@ const handleLogin = () => {
             <div className='registrationPage'>
                 <h2>Login</h2>
                 <div className='formContainer'>
-                    <input className='submissionInput' type='test' placeholder='Enter Email' onChange={(e) => {
-                        setEmail(e.target.value)}} value={email}></input>
-                    <input className='submissionInput' type='test' placeholder='Enter Password' onChange={(e) => {
+                    <input className='submissionInput' type='text' placeholder='Enter Username' onChange={(e) => {
+                        setUsername(e.target.value)}} value={username}></input>
+                    <input className='submissionInput' type='password' placeholder='Enter Password' onChange={(e) => {
                         setPassword(e.target.value)}} value={password}></input>
                     <button onClick={handleLogin} className='submissionButton' type='button'>Login</button>
                 </div>

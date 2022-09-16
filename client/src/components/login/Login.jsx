5bf09acd43a0c85870e4c4f02f2fad219086bda3
AlexBrown4020 <balex3497@gmail.com>
2022-09-16 11:26:27 +0900

    Feat: Apply registration styles to login component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Navbar } from '../navbar/Navbar';
 
+
 export const Login = () => {
     return (
         <div>
             <Navbar/>
-            <h2>Login</h2>
+            <div className='registrationPage'>
+                <h2>Login</h2>
+                <div className='formContainer'>
+                    <input className='submissionInput' type='test' placeholder='Enter Email'></input>
+                    <input className='submissionInput' type='test' placeholder='Enter Password'></input>
+                    <button className='submissionButton' type='button'>Login</button>
+                </div>
+            </div>
         </div>
     )
 };

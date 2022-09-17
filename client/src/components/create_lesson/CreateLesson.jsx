6da0a3767720907086e090ebeff6b588dc6b9dc4
AlexBrown4020 <balex3497@gmail.com>
2022-09-17 14:11:05 +0900

    Feat: Prevent non-admins from accessing create lesson link

--- client/src/components/create_lesson/CreateLesson.jsx
+++ client/src/components/create_lesson/CreateLesson.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Navbar } from '../navbar/Navbar';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export const CreateLesson = () => {
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const auth = localStorage.getItem('user');
+        if (!JSON.parse(auth).isAdmin) {
+            navigate('/')
+        }
+    });
+
 
     return (
         <div>

07bcf74119c1f48e4fac988aeaa2cf6abee49da9
AlexBrown4020 <balex3497@gmail.com>
2022-09-17 15:34:23 +0900

    Feat: Conditional render if the user is an admin

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import './navbar.css';
-import {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
-    const auth = localStorage.getItem('user');
+    let auth = localStorage.getItem('user');
+    if (!auth) {
+        auth = {
+            isAdmin: false
+        }
+        console.log(auth)
+    }
 
     const navigate = useNavigate();
     const logout = () => {
@@ -16,12 +21,15 @@ export const Navbar = () => {
             <div className='navContainer'>
                 <Link className='logo' to={'/'}>Life In Balance</Link>
                 <ul className='navItems'>
-                { JSON.parse(auth).isAdmin ? <Link className='navLink' to={'/create_lesson'}>Create Lesson</Link> 
-                : <></>
+
+                { 
+                    !auth ?  <Link className='navLink' to={'/create_lesson'}>Create Lesson</Link> 
+                    : <></>  
                 }
+                    
 
                 {   
-                    auth ? <Link className='navLink' onClick={logout} to='/'>Logout</Link> 
+                    !auth ? <Link className='navLink' onClick={logout} to='/'>Logout</Link> 
                     : <>
                         <Link className='navLink'to={'/register'}>Register</Link>
                         <Link className='navLink' to={'/login'}>Login</Link>

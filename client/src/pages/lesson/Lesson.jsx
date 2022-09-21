d15e26544dd08ab9368e4b10bdc3ee4201a7da0a
AlexBrown4020 <balex3497@gmail.com>
2022-09-21 16:47:53 +0900

    Refactor: Fix indentation for lesson

--- client/src/pages/lesson/Lesson.jsx
+++ client/src/pages/lesson/Lesson.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { Navbar } from '../../components/navbar/Navbar';
 import useFetch from '../../hooks/useFetch';
 import '../list/list.css';
+import './lesson.css';
 
 export const Lesson = () => {
   const param = useParams();
@@ -12,36 +13,34 @@ export const Lesson = () => {
   return (
     <div>
       <Navbar/>
-      <div className='lessonList'>
+      <div className='mainContainer'>
       { loading ? (
         'Loading, please wait'
       ) : (
         <div key={data._id} className='lessonContainer' >
-        <div >
-            <img alt='default placehodler of group stretching' className='lessonImg' src='https://i0.wp.com/www.yogabasics.com/yogabasics2017/wp-content/uploads/2014/12/gentle-yoga-class.jpeg?w=1080&ssl=1'></img>
+          <div >
+              <img alt='default placehodler of group stretching' className='lessonImg' src='https://i0.wp.com/www.yogabasics.com/yogabasics2017/wp-content/uploads/2014/12/gentle-yoga-class.jpeg?w=1080&ssl=1'></img>
+          </div>
+          <h2>{data.title}</h2>
+          <div className='lessonContent'>
+              <div className='contentBlock'>
+                  <p className='lessonText'>Day: </p> 
+                  {
+                    data.date === undefined ? <p>Loading date...</p> : <p>{data.date.slice(0,10)}</p>
+                  }
+              </div>
+              <div className='contentBlock'>
+                  <p className='lessonText'>Time: </p> 
+                  {
+                    data.date === undefined ? <p>Loading date...</p> : <p>{data.date.slice(11,19)}</p>
+                  }
+              </div>
+              <div className='contentBlock'>
+                  <p className='lessonText'>Description: </p> 
+                  <p>{data.description}</p>
+              </div>
+          </div>
         </div>
-        <h2>
-            {data.title}
-        </h2>
-        <div className='lessonContent'>
-            <div className='contentBlock'>
-                <p className='lessonText'>Day: </p> 
-                {
-                  data.date === undefined ? <p>Loading date...</p> : <p>{data.date.slice(0,10)}</p>
-                }
-            </div>
-            <div className='contentBlock'>
-                <p className='lessonText'>Time: </p> 
-                {
-                  data.date === undefined ? <p>Loading date...</p> : <p>{data.date.slice(11,19)}</p>
-                }
-            </div>
-            <div className='contentBlock'>
-                <p className='lessonText'>Description: </p> 
-                <p>{data.description}</p>
-            </div>
-        </div>
-      </div>
       )}
       </div>
     </div>

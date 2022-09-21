61c03967884b351b5484a9fa588f8c9c284df71b
AlexBrown4020 <balex3497@gmail.com>
2022-09-21 11:59:27 +0900

    Feat: Show individual lesson on click

--- client/src/pages/lesson/Lesson.jsx
+++ client/src/pages/lesson/Lesson.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { Navbar } from '../../components/navbar/Navbar';
+import useFetch from '../../hooks/useFetch';
 
 export const Lesson = () => {
+  const param = useParams();
+  const {data, loading, error} = useFetch(`/lessons/${param.id}`);
+
   return (
-    <div>Lesson</div>
-  )
-};
+    <div>
+      <Navbar/>
+      { loading ? (
+        'Loading, please wait'
+      ) : (
+        <div key={data._id} className='lessonContainer' >
+        <div >
+            <img alt='default placehodler of group stretching' className='lessonImg' src='https://i0.wp.com/www.yogabasics.com/yogabasics2017/wp-content/uploads/2014/12/gentle-yoga-class.jpeg?w=1080&ssl=1'></img>
+        </div>
+        <h2>
+            {data.title}
+        </h2>
+        <div className='lessonContent'>
+            <div className='contentBlock'>
+                <p className='lessonText'>When: </p> 
+                <p>{data.date}</p>
+            </div>
+            <div className='contentBlock'>
+                <p className='lessonText'>Description: </p> 
+                <p>{data.description}</p>
+            </div>
+        </div>
+      </div>
+      )}
+
+    </div>
+  )};
